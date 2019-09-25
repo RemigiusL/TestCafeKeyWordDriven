85ec6bae2a17a50addd234165295c0d364794b27
Remi L <remi@processdrive.com>
2019-09-25 10:46:12 +0530

    updated all the actions with syntax and the functionality description

--- Keyword-Driven.js
+++ Keyword-Driven.js
@@ -11,7 +11,11 @@ const getLogAppender = new logAppender()
 const logger = getLogger();
         logger.level = 'debug';
         getLogAppender.info()
-
+/**
+ * Description of the functionality
+    * Testcafe Keyword Driven Framework is a type of Functional Automation Testing Framework which is also known as Table-Driven testing or Action Word-based testing. 
+    * A node.js tool to automate end-to-end web testing.
+ **/
 
 fixture `Getting Started`
 
@@ -22,7 +26,7 @@ var XLSX = require('xlsx')
     var sheet_name_list = workbook.SheetNames;
     var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
 try {
-    test('Keyword-Driven',  async t => {
+    test('Keyword-Driven Framework',  async t => {
         await t.maximizeWindow()
         for (let i = 0; i < xlData.length; i++) {
             let element = xlData[i]
@@ -37,17 +41,24 @@ try {
                 default:
                         break;
             }
-            
             switch (element.Keyword) {
-                case "navigateTo": 
+                case "navigateTo": //t.navigateTo( url )
                     await t[element.Keyword](element.Parameter)
-                    logger.info(element.Keyword +" Url "+ element.Parameter + " - After test execution, actual test result should be navigated")
+                    logger.info(element.Keyword + " Url " + element.Parameter + " - After test execution, actual test result should be navigated")
                     break;
-                case "click": 
+                case "click": //t.click( selector [, options] )
                     await t[element.Keyword](element.LocatorType(element.LocatorValue))
                     logger.info(element.Keyword + " " + element.LocatorValue +" - After test execution, actual test result should be clicked")
                     break;
-                case "typeText":
+                case "doubleClick": //t.doubleClick( selector [, options] )
+                    await t[element.Keyword](element.LocatorType(element.LocatorValue))
+                    logger.info(element.Keyword + element.LocatorType + element.LocatorValue +" - After test execution, actual test result should be doubleClicked")
+                    break;
+                case "rightClick": //t.rightClick( selector [, options] )
+                    await t[element.Keyword](element.LocatorType(element.LocatorValue))
+                    logger.info(element.Keyword + element.LocatorType + element.LocatorValue +" - After test execution, actual test result should be rightClicked")
+                    break;
+                case "typeText": //t.typeText( selector, text [, options] )
                     await t[element.Keyword](element.LocatorType(element.LocatorValue), element.Parameter, { speed: 1 })
                     logger.info(element.Keyword + " " + element.Parameter +" - After test execution, actual test result should be filled")
                     break;
@@ -64,16 +75,35 @@ try {
                     await t [element.Keyword](interfaceSelect.find('option').withText(element.Parameter))
                     logger.info(element.Keyword + " " + element.Parameter +" - After test execution, actual test result should be selected")
                     break;
-                case "withText":
+                case "withText": //.click(Selector('a').withText('Glemt passord?'))
                     element.Keyword ="click"
                     await t [element.Keyword](element.LocatorType(element.LocatorValue).withText(element.Parameter))
                     logger.info(element.Keyword + " " + element.LocatorValue +" withText " +element.Parameter +" - After test execution, actual test result should be clicked by withText")
                     break;
-                case "pressKey": 
+                case "pressKey": //t.pressKey( keys [, options] )
                     await t[element.Keyword](element.Parameter);
                     logger.info(element.Keyword + " " + element.Parameter +" - After test execution, actual test result should be pressed the specific key")
                     break;
-               
+                case "hover": //t.hover( selector [, options] )
+                    await t[element.Keyword](element.LocatorType(element.LocatorValue))
+                    logger.info(element.Keyword + element.LocatorType + element.Parameter +" - After test execution, actual test result should be hover the specific field")
+                    break;
+                case "selectText": //t.selectText( selector [, startPos] [, endPos] [, options] )
+                    await t[element.Keyword](element.LocatorType(element.LocatorValue), element.Parameter, { speed: 1 })
+                    logger.info(element.Keyword + element.LocatorType + element.Parameter +" - After test execution, actual test result should be hover the specific field")
+                    break;
+                case "setFilesToUpload": //t.setFilesToUpload( selector, filePath )
+                    await t[element.Keyword](element.LocatorType(element.LocatorValue), element.Parameter)
+                    logger.info(element.Keyword + element.LocatorType + element.Parameter +" - After test execution, actual test result should be uploaded files into the specific app")
+                    break;
+                case "resizeWindow": //t.resizeWindow( width, height )
+                    await t[element.Keyword](element.Parameter)
+                    logger.info(element.Keyword + "  " + element.Parameter + " - After test execution, actual test result should be resizedwindow")
+                    break;
+                case "drag": //t.drag( selector, dragOffsetX, dragOffsetY [, options] )
+                    await t[element.Keyword](element.LocatorType(element.LocatorValue), element.Parameter, { offsetX: 10, offsetY: 10 })
+                    logger.info(element.Keyword + element.LocatorType + element.Parameter +" - After test execution, actual test result should be draged element into the position")
+                    break;
                 default:
                     return;
             }
