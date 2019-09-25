3f08b90b86652d9a212fdc880d5e8949c1a452d4
Remi L <remi@processdrive.com>
2019-09-25 14:27:21 +0530

    const variable added for locatorType

--- Keyword-Driven.js
+++ Keyword-Driven.js
@@ -30,7 +30,8 @@ try {
         await t.maximizeWindow()
         for (let i = 0; i < xlData.length; i++) {
             let element = xlData[i]
-
+            
+            const LocatorType = element.LocatorType
             switch (element.LocatorType) {
                 case "XPath":
                         element.LocatorType = XPath
@@ -44,57 +45,57 @@ try {
             switch (element.Keyword) {
                 case "navigateTo": //t.navigateTo( url )
                     await t[element.Keyword](element.Parameter)
-                    logger.info(element.Keyword + " Url " + element.Parameter + " - After test execution, actual test result should be navigated")
+                    logger.info(element.Keyword +" Url "+ element.Parameter + " - After test execution, actual test result should be navigated")
                     break;
                 case "click": //t.click( selector [, options] )
                     await t[element.Keyword](element.LocatorType(element.LocatorValue))
-                    logger.info(element.Keyword + " " + element.LocatorValue +" - After test execution, actual test result should be clicked")
+                    logger.info(element.Keyword+" "+LocatorType+" "+element.LocatorValue +" - After test execution, actual test result should be clicked")
                     break;
                 case "doubleClick": //t.doubleClick( selector [, options] )
                     await t[element.Keyword](element.LocatorType(element.LocatorValue))
-                    logger.info(element.Keyword + element.LocatorType + element.LocatorValue +" - After test execution, actual test result should be doubleClicked")
+                    logger.info(element.Keyword+" "+LocatorType+" "+element.LocatorValue +" - After test execution, actual test result should be doubleClicked")
                     break;
                 case "rightClick": //t.rightClick( selector [, options] )
                     await t[element.Keyword](element.LocatorType(element.LocatorValue))
-                    logger.info(element.Keyword + element.LocatorType + element.LocatorValue +" - After test execution, actual test result should be rightClicked")
+                    logger.info(element.Keyword+" "+LocatorType+" "+element.LocatorValue +" - After test execution, actual test result should be rightClicked")
                     break;
                 case "typeText": //t.typeText( selector, text [, options] )
                     await t[element.Keyword](element.LocatorType(element.LocatorValue), element.Parameter, { speed: 1 })
-                    logger.info(element.Keyword + " " + element.Parameter +" - After test execution, actual test result should be filled")
+                    logger.info(element.Keyword+" "+LocatorType+" "+element.Parameter +" - After test execution, actual test result should be filled with text")
                     break;
                 case "clear":
                     element.Keyword = "click"
                     await t [element.Keyword](element.LocatorType(element.LocatorValue));
                     await t.pressKey('ctrl+a delete');
-                    logger.info(element.Keyword + " " + element.LocatorValue +" - After test execution, actual test result should be cleared")
+                    logger.info(element.Keyword+" "+LocatorType+" "+element.LocatorValue +" - After test execution, actual test result should be cleared")
                     break;
                 case "select":
                     element.Keyword ="click"
                     const interfaceSelect = (element.LocatorType(element.LocatorValue));
                     await t [element.Keyword](interfaceSelect)
                     await t [element.Keyword](interfaceSelect.find('option').withText(element.Parameter))
-                    logger.info(element.Keyword + " " + element.Parameter +" - After test execution, actual test result should be selected")
+                    logger.info(element.Keyword+" "+LocatorType+" "+element.Parameter +" - After test execution, actual test result should be selected")
                     break;
                 case "withText": //.click(Selector('a').withText('Glemt passord?'))
                     element.Keyword ="click"
                     await t [element.Keyword](element.LocatorType(element.LocatorValue).withText(element.Parameter))
-                    logger.info(element.Keyword + " " + element.LocatorValue +" withText " +element.Parameter +" - After test execution, actual test result should be clicked by withText")
+                    logger.info(element.Keyword+" "+LocatorType+" "+element.LocatorValue +" withText " +element.Parameter +" - After test execution, actual test result should be clicked by withText")
                     break;
                 case "pressKey": //t.pressKey( keys [, options] )
                     await t[element.Keyword](element.Parameter);
-                    logger.info(element.Keyword + " " + element.Parameter +" - After test execution, actual test result should be pressed the specific key")
+                    logger.info(element.Keyword+" "+LocatorType+" "+element.Parameter +" - After test execution, actual test result should be pressed the specific key")
                     break;
                 case "hover": //t.hover( selector [, options] )
                     await t[element.Keyword](element.LocatorType(element.LocatorValue))
-                    logger.info(element.Keyword + element.LocatorType + element.Parameter +" - After test execution, actual test result should be hover the specific field")
+                    logger.info(element.Keyword+" "+LocatorType+" "+element.Parameter +" - After test execution, actual test result should be hover the specific field")
                     break;
                 case "selectText": //t.selectText( selector [, startPos] [, endPos] [, options] )
                     await t[element.Keyword](element.LocatorType(element.LocatorValue), element.Parameter, { speed: 1 })
-                    logger.info(element.Keyword + element.LocatorType + element.Parameter +" - After test execution, actual test result should be hover the specific field")
+                    logger.info(element.Keyword+" "+LocatorType+" "+element.Parameter +" - After test execution, actual test result should be selectText the specific field")
                     break;
                 case "setFilesToUpload": //t.setFilesToUpload( selector, filePath )
                     await t[element.Keyword](element.LocatorType(element.LocatorValue), element.Parameter)
-                    logger.info(element.Keyword + element.LocatorType + element.Parameter +" - After test execution, actual test result should be uploaded files into the specific app")
+                    logger.info(element.Keyword+" "+LocatorType+" "+element.Parameter +" - After test execution, actual test result should be uploaded files into the specific app")
                     break;
                 case "resizeWindow": //t.resizeWindow( width, height )
                     await t[element.Keyword](element.Parameter)
@@ -102,7 +103,7 @@ try {
                     break;
                 case "drag": //t.drag( selector, dragOffsetX, dragOffsetY [, options] )
                     await t[element.Keyword](element.LocatorType(element.LocatorValue), element.Parameter, { offsetX: 10, offsetY: 10 })
-                    logger.info(element.Keyword + element.LocatorType + element.Parameter +" - After test execution, actual test result should be draged element into the position")
+                    logger.info(element.Keyword+" "+LocatorType+" "+element.Parameter +" - After test execution, actual test result should be draged element into the position")
                     break;
                 default:
                     return;
